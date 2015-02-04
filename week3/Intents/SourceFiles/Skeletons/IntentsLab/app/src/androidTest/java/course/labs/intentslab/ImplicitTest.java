05347f9da7f27c32d5e825d3f351f324c28f1136
perwalst <per.jorgen.walstrom@finn.no>
2015-02-04 21:45:28 +0100

    week 3, lab 2

--- week3/Intents/SourceFiles/Skeletons/IntentsLab/app/src/androidTest/java/course/labs/intentslab/ImplicitTest.java
+++ week3/Intents/SourceFiles/Skeletons/IntentsLab/app/src/androidTest/java/course/labs/intentslab/ImplicitTest.java
@@ -1,6 +1,7 @@
 package course.labs.intentslab;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 import com.robotium.solo.Solo;
 
@@ -9,6 +10,7 @@ import course.labs.intentslab.ActivityLoaderActivity;
 public class ImplicitTest extends
 		ActivityInstrumentationTestCase2<ActivityLoaderActivity> {
 	private Solo solo;
+    static private final String TAG = "Lab-Intents";
 
 	public ImplicitTest() {
 		super(ActivityLoaderActivity.class);
@@ -27,6 +29,7 @@ public class ImplicitTest extends
 	// Executes the ImplicitTest
 	public void testRun() {
 
+        Log.i(TAG, "Entered testRun()");
 		// =================== Section One =====================
 
 		// Wait for activity: 'course.labs.intentslab.ActivityLoaderActivity'
@@ -35,20 +38,25 @@ public class ImplicitTest extends
 				"Section One:" +
 				"ActivityLoaderActivity did not load correctly",
 				solo.waitForActivity(ActivityLoaderActivity.class));
+        Log.i(TAG, "Entered testRun() 2");
 
 		// Click on Implicit Activation Button
 		solo.clickOnView(solo
 				.getView(R.id.implicit_activation_button));
 
+        Log.i(TAG, "Entered testRun() 3");
 
-		// Wait for activity: 'com.android.internal.app.ChooserActivity'
+
+        // Wait for activity: 'com.android.internal.app.ChooserActivity'
 		assertTrue(
 				"ImplicitTest:" +
 				"Section One:" +
 				"ChooserActivity was not launched correctly",
 				solo.waitForActivity("ChooserActivity"));
 
-		// Click on MyBrowser
+        Log.i(TAG, "Entered testRun() 4");
+
+        // Click on MyBrowser
 		solo.clickInList(2, 0);
 		
 		
